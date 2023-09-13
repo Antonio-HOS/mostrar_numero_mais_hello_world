b42049d78e4f7355ed71fa76d0a1c00687f0674e
Antonio Henrique <144703171+Antonio-HOS@users.noreply.github.com>
2023-09-13 18:17:36 -0300

    Add files via upload

---
+++ aula1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int var;
+  int var2 = 50;
+  printf("ola mundoooooooooooooooooooooooooooooo: \n");
+  scanf("%i", &var);
+
+  
+  printf("Hello World %i, %i\n", var,var2);
+  
+  return 0;
+}
